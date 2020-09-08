7c75e0366d0c074a3f094cd12acb19a0b1af4c74
P4yin <60865374+P4yin@users.noreply.github.com>
2020-09-07 23:11:42 -0400

    Delete AminoAcidConverter.h

--- AminoAcidConverter.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-using namespace std;
-
-class AminoAcidConverter
-{
-public:
-	string mRNA_to_acid(string codon, int length); //turns the mRNA codons into their respective amino acids
-};
